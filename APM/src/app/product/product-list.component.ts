b8561c2f19866a26e60588755aa3162700b4c69c
brunotarghetta <brunotarghetta@gmail.com>
2022-10-18 17:30:24 -0300

    Data binding and pipes

--- APM/src/app/product/product-list.component.ts
+++ APM/src/app/product/product-list.component.ts
@@ -7,6 +7,8 @@ import { Component} from "@angular/core";
 
 export class ProductListComponent {
     pageTitle: string = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
             "productId": 1,
@@ -29,4 +31,10 @@ export class ProductListComponent {
             "imageUrl": "assets/images/garden_cart.png"
           }
     ];
+    showImage: boolean = false;
+    listFilter: string = "cart";
+    
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }
 }
