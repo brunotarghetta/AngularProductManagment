94e357f576da0d8e097f4077f226a02528c9a94d
brunotarghetta <brunotarghetta@gmail.com>
2022-10-25 11:42:18 -0300

    Mdodules Imports, declaration, exports, bootstrap array

---
+++ APM/src/app/products/product.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+import ConvertToSpacesPipe from '../shared/convert-to-spaces.pipe';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-detail.guard';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [ 
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    ProductDetailComponent],
+  imports: [    
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent, canActivate:[ProductDetailGuard]}
+
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule { }
